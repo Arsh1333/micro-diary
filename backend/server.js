0d974b0760a689f731f552bf38c6a2591855bf88
Arsh <pawar.arsh1333@gmail.com>
2025-05-24 23:13:00 +0530

    adding limits

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import cors from "cors";
 import connectDb from "./db/connectDb.js";
 import entriesRouter from "./routes/Entries.routes.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
